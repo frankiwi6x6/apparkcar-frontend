1b4d18d3a782dbc645a02774a83df2b6b0554d4f
frankiwi6x6 <fra.reyesj@duocuc.cl>
2023-12-10 19:21:49 -0300

    Frontend updated

--- src/components/ProfileContent.tsx
+++ src/components/ProfileContent.tsx
@@ -1,7 +1,30 @@
 // Importa las dependencias necesarias
-import React from 'react';
-import { IonRow, IonCol, IonTitle, IonCard, IonCardTitle, IonCardSubtitle, IonText } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { IonRow, IonCol, IonTitle, IonCard, IonCardTitle, IonCardSubtitle, IonText, IonSpinner } from '@ionic/react';
+import { Rating } from 'react-simple-star-rating';
+const NoDataMessage: React.FC = () => (
+    <div>
+        <IonText>No hay datos disponibles.</IonText>
+    </div>
+);
 
+const DelayedSpinnerAndMessage: React.FC = () => {
+    const [showSpinner, setShowSpinner] = useState(true);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setShowSpinner(false);
+        }, 3000);
+
+        return () => clearTimeout(timerId);
+    }, []);
+
+    return showSpinner ? (
+        <IonSpinner></IonSpinner>
+    ) : (
+        <NoDataMessage />
+    );
+};
 interface Props {
     user: any;
     calificacion: any;
@@ -25,70 +48,105 @@ const ProfileContent: React.FC<Props> = ({
 }) => {
     return (
         <div>
-            <IonRow>
-                <IonCol>
-                    <IonTitle>Calificación</IonTitle>
-                    {calificacion && calificacion.promedio_calificacion !== null ? (
-                        <IonTitle>{calificacion.promedio_calificacion}</IonTitle>
-                    ) : (
-                        <IonText>Sin calificaciones</IonText>
-                    )}
-                </IonCol>
-                <IonCol>
-                    {user.es_cliente === true ? (
-                        <div>
-                            <IonTitle>Reservas</IonTitle>
-                            {reservas && reservas.length > 0 ? (
-                                <IonTitle>{reservas.length}</IonTitle>
-                            ) : (
-                                <IonText>Sin reservas</IonText>
-                            )}
-                        </div>
-                    ) : (
-                        <div>
-                            <IonTitle>Estacionamientos</IonTitle>
-                            {estacionamientos && estacionamientos.length > 0 ? (
-                                <IonTitle>{estacionamientos.length}</IonTitle>
-                            ) : (
-                                <IonText>Sin estacionamientos</IonText>
-                            )}
-                        </div>
-                    )}
-                </IonCol>
-            </IonRow>
-            <IonRow>
-                <IonCol className=''>
-                    {user.es_cliente === true ? (
-                        <div>
-                            <IonTitle>Estacionamientos listados</IonTitle>
-                            {reservas.map((reserva: any, index: number) => (
-                                <IonCard key={index}>
-                                    <IonCardTitle> <strong>Reserva: </strong>{reserva.id}</IonCardTitle>
-                                    <IonCardSubtitle> <strong>Estacionamiento: </strong>{reserva.id_estacionamiento}</IonCardSubtitle>
-                                    <IonCardSubtitle> <strong>Inicio: </strong> {formatDate(reserva.fecha_inicio)}</IonCardSubtitle>
-                                    <IonCardSubtitle> <strong>Fin: </strong> {reserva.fecha_fin ? formatDate(reserva.fecha_fin) : "En curso..."}</IonCardSubtitle>
-                                    <IonCardSubtitle> <strong>Valor: </strong> ${formatPrice(calculateTotalPrice(calculateHours(reserva.fecha_inicio, reserva.fecha_fin), reserva.valor))}</IonCardSubtitle>
-                                </IonCard>
-                            ))}
-                        </div>
-                    ) : (
-                        <div>
-                            <IonTitle>Estacionamientos listados</IonTitle>
-                            {estacionamientos.map((estacionamiento: any, index: number) => (
-                                <IonCard key={index}>
-                                    <IonCardTitle><strong>Estacionamiento: </strong>{estacionamiento.id}</IonCardTitle>
-                                    <IonCardSubtitle>{estacionamiento.titulo}</IonCardSubtitle>
-                                    <IonCardSubtitle><strong>Descripción: </strong>{estacionamiento.descripcion}</IonCardSubtitle>
-                                    <IonCardSubtitle><strong>Tipo: </strong>{estacionamiento.tipo}</IonCardSubtitle>
-                                    <IonCardSubtitle><strong>Capacidad disponible:</strong> {estacionamiento.capacidad}</IonCardSubtitle>
-                                    <IonCardSubtitle><strong>Estado: </strong> {estacionamiento.estado}</IonCardSubtitle>
-                                </IonCard>
-                            ))}
-                        </div>
-                    )}
-                </IonCol>
-            </IonRow>
-        </div>
+                        <IonRow>
+                            <IonCol>
+                                <IonTitle>Calificación</IonTitle>
+                                {calificacion && calificacion.promedio_calificacion !== null ? (
+                                    <div>
+                                        <IonTitle>{calificacion.promedio_calificacion}</IonTitle>
+                                        <Rating
+                                            onClick={() => { null }}
+                                            initialValue={calificacion.promedio_calificacion}
+                                            allowFraction={true}
+                                            allowHover={false}
+                                        />
+                                    </div>
+                                ) : (
+                                    <IonSpinner></IonSpinner>
+                                )}
+                            </IonCol>
+                            <IonCol>
+                                {user.es_cliente === true ? (
+                                    <div>
+                                        <IonTitle>Reservas</IonTitle>
+                                        {reservas ? (
+                                            reservas.length > 0 ? (
+                                                <IonTitle>{reservas.length}</IonTitle>
+                                            ) : (
+                                                <DelayedSpinnerAndMessage />
+                                            )
+                                        ) : (
+                                            <NoDataMessage />
+                                        )}
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <IonTitle>Estacionamientos</IonTitle>
+                                        {estacionamientos ? (
+                                            estacionamientos.length > 0 ? (
+                                                <IonTitle>{estacionamientos.length}</IonTitle>
+                                            ) : (
+                                                <DelayedSpinnerAndMessage />
+                                            )
+                                        ) : (
+                                            <NoDataMessage />
+                                        )}
+                                    </div>
+                                )}
+                            </IonCol>
+                        </IonRow>
+                        <IonRow>
+                            <IonCol className=''>
+                                {user.es_cliente === true ? (
+                                    reservas ? (
+                                        reservas.length > 0 ? (
+                                            <div>
+                                                <IonTitle>Reservas Realizadas</IonTitle>
+                                                {reservas.map((reserva: any, index: number) => (
+                                                    <IonCard key={index}>
+                                                        <IonCardTitle> <strong>Reserva: </strong>{reserva.id}</IonCardTitle>
+                                                        <IonCardSubtitle> <strong>Estacionamiento: </strong>{reserva.id_estacionamiento}</IonCardSubtitle>
+                                                        <IonCardSubtitle> <strong>Inicio: </strong> {formatDate(reserva.fecha_inicio)}</IonCardSubtitle>
+                                                        <IonCardSubtitle> <strong>Fin: </strong> {reserva.fecha_fin ? formatDate(reserva.fecha_fin) : "En curso..."}</IonCardSubtitle>
+                                                        <IonCardSubtitle> <strong>Valor: </strong> ${formatPrice(calculateTotalPrice(calculateHours(reserva.fecha_inicio, reserva.fecha_fin), reserva.valor))}</IonCardSubtitle>
+                                                    </IonCard>
+                                                ))}
+                                            </div>
+                                        ) : (
+                                            <IonSpinner></IonSpinner>
+                                        )
+                                    ) : (
+                                        <NoDataMessage />
+                                    )
+                                ) : (
+                                    estacionamientos ? (
+                                        estacionamientos.length > 0 ? (
+                                            <div>
+                                                <IonTitle>Estacionamientos listados</IonTitle>
+                                                {estacionamientos.map((estacionamiento: any, index: number) => (
+                                                    <IonCard key={index}>
+                                                        <IonCardTitle><strong>Estacionamiento: </strong>{estacionamiento.id}</IonCardTitle>
+                                                        <IonCardSubtitle>{estacionamiento.titulo}</IonCardSubtitle>
+                                                        <IonCardSubtitle><strong>Descripción: </strong>{estacionamiento.descripcion}</IonCardSubtitle>
+                                                        <IonCardSubtitle><strong>Tipo: </strong>{estacionamiento.tipo}</IonCardSubtitle>
+                                                        <IonCardSubtitle><strong>Capacidad disponible:</strong> {estacionamiento.capacidad}</IonCardSubtitle>
+                                                        <IonCardSubtitle><strong>Estado: </strong> {`${estacionamiento.estado}`}</IonCardSubtitle>
+                                                    </IonCard>
+                                                ))}
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                <IonTitle>Cargando estacionamientos...</IonTitle>
+                                                <DelayedSpinnerAndMessage/>
+                                            </div>
+                                        )
+                                    ) : (
+                                        <NoDataMessage />
+                                    )
+                                )}
+                            </IonCol>
+                        </IonRow>
+                    </div>
     );
 };
 
